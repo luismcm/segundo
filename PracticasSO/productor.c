d3975c8b7c58dd1ee343048e236f177c065280ff
luismcm <73791522+luismcm@users.noreply.github.com>
2021-12-15 22:11:35 +0100

    Create productor.c

---
+++ PracticasSO/productor.c
@@ -0,0 +1,9 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#define ARCHIVO_FIFO "Comunicacion FIFO"
